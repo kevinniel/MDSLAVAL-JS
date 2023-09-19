22f1d922bf56b3ab39864e37e4414a6fb2912874
kevinniel <k.niel.pro@gmail.com>
2023-09-19 11:07:34 +0200

    update

---
+++ readme.md
@@ -0,0 +1,5 @@
+## TP 1
+
+Dans le fichier ```script.js```, réaliser les deux actions suivantes : 
+* Sélectionner toutes les marques et les compter
+* Sélectioner toutes les cases de nombre, et faire le total
--- script.js
+++ script.js
@@ -2,6 +2,4 @@ console.log("-----------------------------------");
 console.log("script.js");
 
 // Sélectionner toutes les marques et les compter
-
-
 // Sélectioner toutes les cases de nombre, et faire le total

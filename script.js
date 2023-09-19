78feb665eacfc0eb4b500160a70d51d589265459
kevinniel <k.niel.pro@gmail.com>
2023-09-19 11:43:32 +0200

    update

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ console.log("-----------------------------------");
 console.log("script.js");
 
 // Sélectionner toutes les marques et les compter
+const brands = document.querySelectorAll(".marque");
+console.log("nombre de marques : ", brands.length)
 // Sélectioner toutes les cases de nombre, et faire le total
+const numbers = document.querySelectorAll(".nombre")
+let sum = 0;
+for (let i = 0; i < numbers.length; i++) {
+    console.log( numbers[i].innerHTML );
+    sum += parseInt(numbers[i].innerHTML);
+}
+console.log("somme : ", sum);
+
+

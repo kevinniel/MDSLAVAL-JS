89585c0dff13f5e6010b68f48007480789613fdd
kevinniel <k.niel.pro@gmail.com>
2023-09-19 11:06:01 +0200

    update

---
+++ course.js
@@ -0,0 +1,30 @@
+console.log("course.js");
+
+// sélection d'un élément
+const table = document.querySelector("table");
+// sélection de plusieurs éléments
+const tds = document.querySelectorAll("td");
+
+// affichage des sélections
+// console.log(table);
+// console.log(tds);
+
+// afficher la case 3 du tableau tds :
+// console.log(tds[2]);
+
+// affiche la case qui contient "champion"
+// console.log(tds[7]);
+
+
+// Afficher tous les tds 1 par 1, sans tableau :
+
+// Première manière
+// tds.forEach((td) => 
+//     console.log(td)
+// )
+
+// deuxième manière
+// for (let i = 0; i < tds.length; i++) {
+//     console.log( tds[i] );
+// }
+
--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("tata");
+console.log("-----------------------------------");
+console.log("script.js");
+
+// Sélectionner toutes les marques et les compter
+
+
+// Sélectioner toutes les cases de nombre, et faire le total
